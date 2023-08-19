7ab19288550071b3954bf5f81b22d412dc453c2b
sohel5G <contactsohelrana@gmail.com>
2023-08-19 18:04:29 +0600

    right side bar adding result

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,14 @@
 document.getElementById('btn-triangle').addEventListener('click', function(){
     let baseInputValue = getInputValueById('triangle-base');
     let heightInputValue = getInputValueById('triangle-height');
+    if(isNaN(baseInputValue) || isNaN(heightInputValue)){
+        alert('Please Enter a Valid Number');
+        return;
+    };
     let area = 0.5 * baseInputValue * heightInputValue;
     setElementValueById('triangle-total', area);
+    
+    addToCalculationArea('Triangle', area)
 });
 
 document.getElementById('btn-rectangle').addEventListener('click', function(){
@@ -10,6 +16,8 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
     let heightInputValue = getInputValueById('rectangle-height');
     let area = baseInputValue * heightInputValue;
     setElementValueById('rectangle-total', area);
+
+    addToCalculationArea('Rectangle', area)
 });
 
 document.getElementById('btn-parallelogram').addEventListener('click', function(){
@@ -17,6 +25,8 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
     let heightInputValue = getInputValueById('parallelogram-height');
     let area = baseInputValue * heightInputValue;
     setElementValueById('parallelogram-total', area);
+
+    addToCalculationArea('Parallelogram', area)
 });
 
 
@@ -29,6 +39,8 @@ document.getElementById('btn-ellipse').addEventListener('click', function(){
     let minorRadius = getInputValueById('minor-radius');
     let area = Math.PI * majorRadius * minorRadius;
     setElementValueById('ellipse-total', area.toFixed(2));
+
+    addToCalculationArea('Ellipse', area.toFixed(2))
 });
 
 
--- js/functions.js
+++ js/functions.js
@@ -10,3 +10,14 @@ function setElementValueById(id, value){
     getElement.innerText = value;
 }
 
+function addToCalculationArea(areaName, totalValue){
+    let calculationEntry = document.getElementById('calculation-entry');
+    let countElement = calculationEntry.childElementCount;
+    let p = document.createElement('p');
+    p.innerHTML = `
+        ${countElement + 1})  ${areaName} - ${totalValue} cm <sup>2</sup> <button class="pb-1 px-2 bg-[#1090D8] text-white rounded-md text-sm">convert</button>
+    `
+    p.classList.add('mt-2');
+    calculationEntry.appendChild(p);
+}
+
