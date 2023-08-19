3dc9115f6fc3711a541d782bbe2b1be87b0b8da4
sohel5G <contactsohelrana@gmail.com>
2023-08-19 16:06:26 +0600

    2 card done

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,29 @@
 document.getElementById('btn-triangle').addEventListener('click', function(){
-    let baseInputValue = getInputValueById('input-triangle-base');
-    let heightInputValue = getInputValueById('input-triangle-height');
+    let baseInputValue = getInputValueById('triangle-base');
+    let heightInputValue = getInputValueById('triangle-height');
     let area = 0.5 * baseInputValue * heightInputValue;
     setElementValueById('triangle-total', area);
 });
+
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    let baseInputValue = getInputValueById('rectangle-base');
+    let heightInputValue = getInputValueById('rectangle-height');
+    let area = baseInputValue * heightInputValue;
+    setElementValueById('rectangle-total', area);
+});
+
+
+
+
+
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    let majorRadius = getInputValueById('major-radius');
+    let minorRadius = getInputValueById('minor-radius');
+    let area = Math.PI * majorRadius * minorRadius;
+    setElementValueById('ellipse-total', area.toFixed(2));
+});
+
+
+
 
 
